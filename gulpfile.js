0d5b3594fdb4cc34218ba57115fac5b4059b1e46
shanelgarcia <60453441+shanelgarcia@users.noreply.github.com>
2021-10-30 11:25:18 -0600

    test

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,9 @@ const {src, dest} = require('gulp')
 
 const static = function(cb)
 {
-    return src('src/static/data/*')
-    .pipe(dest('dist/data'))
+    return (src('src/static/data/*')
+    .pipe(dest('dist/data'))) && src('src/_redirects')
+    .pipe(dest('dist'))
 
     cb()
 }
